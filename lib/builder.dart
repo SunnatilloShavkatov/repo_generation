66fcfe5de8de066c14c1d2214897a1fba8deb058
Sunnatillo Shavkatov <sunnatillo.shavkatov@udevs.io>
2022-07-23 09:26:53 +0500

    package update

--- lib/builder.dart
+++ lib/builder.dart
@@ -1,9 +1,15 @@
+library repo_generator;
+
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
 import 'src/extension_generator.dart';
 import 'src/subclass_generator.dart';
+export 'src/extension_generator.dart';
+export 'src/subclass_generator.dart';
 
-Builder extGen(BuilderOptions options) => SharedPartBuilder([ExtensionGenerator()], 'extension_generator');
+Builder extGen(BuilderOptions options) =>
+    SharedPartBuilder([ExtensionGenerator()], 'extension_generator');
 
-Builder subclassGen(BuilderOptions options) => SharedPartBuilder([SubclassGenerator()], 'subclass_generator');
+Builder subclassGen(BuilderOptions options) =>
+    SharedPartBuilder([SubclassGenerator()], 'subclass_generator');
